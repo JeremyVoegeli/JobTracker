a48ac5fdbde4f619d16026eb51e057d2ec850cf9
Jeremy Voegeli <jeremy.voegeli@uconn.edu>
2026-05-21 20:15:21 -0400

    added packages, created server.js

---
+++ server/server.js
@@ -0,0 +1,20 @@
+//Imports, packages that will be used
+const express = require('express');
+const cors = require('cors');
+const applicationRoutes = require('./route/application');
+
+//defines the app as an express instance
+const app = express();
+const PORT = process.env.PORT || 5000; //checks if port was defined, if not, falls back to 5000
+
+//middleware - allows CORS on all requests and converst request to JS object
+app.use(cors());
+app.use(express.json());
+
+//Any incoming request with this URL gets passed to applicationRoutes file to handle
+app.use('/api/applications', applicationRoutes);
+
+//listens on the specified port
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`)
+});

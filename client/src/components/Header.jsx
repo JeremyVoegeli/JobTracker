6ee62502df3b6ccea9ff9b89fa0522b70671f7c3
Jeremy Voegeli <jeremy.voegeli@uconn.edu>
2026-05-28 16:21:16 -0400

    styling for header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,7 +1,7 @@
 function Header({ onAdd }){
     return (
         <div className="flex mb-6">
-            <h1 className="text-5xl font-bold mr-10 text-white">Job Tracker</h1>
+            <h1 className="text-5xl font-bold mr-10 text-white text-shadow-sm/50 text-shadow-indigo-300">Job Tracker</h1>
             <button className="" onClick={() => onAdd()}>Add Application</button>
         </div>
     );

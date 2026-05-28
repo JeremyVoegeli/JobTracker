ee0b9c3da627a9b4816444a28867a47281df5d1d
Jeremy Voegeli <jeremy.voegeli@uconn.edu>
2026-05-28 19:56:23 -0400

    styling add application button

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -2,7 +2,7 @@ function Header({ onAdd }){
     return (
         <div className="flex mb-6">
             <h1 className="text-5xl font-bold mr-10 text-white text-shadow-sm/50 text-shadow-indigo-300">Job Tracker</h1>
-            <button className="" onClick={() => onAdd()}>Add Application</button>
+            <button className="bg-gradient-to-br from-blue-200 via-indigo-500 to-slate-900 text-white font-semibold px-4 py-2 rounded-full hover:opacity-90 cursor-pointer" onClick={() => onAdd()}>Add Application</button>
         </div>
     );
 }

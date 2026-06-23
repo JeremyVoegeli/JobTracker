459f33cb4ce30adbb1d871458dab264bb9699614
Jeremy Voegeli <jeremy.voegeli@uconn.edu>
2026-06-23 19:43:39 -0400

    tailwind styling on the search bar and filter at the top

--- client/src/components/SearchBar.jsx
+++ client/src/components/SearchBar.jsx
@@ -1,9 +1,9 @@
 function SearchBar({ searchText, searchField, setSearchText, setSearchField }){
 
     return (
-        <div>
-            <input value={searchText} onChange={(event) => setSearchText(event.target.value)}></input>
-            <select name="searchField" value={searchField} onChange={(event) => setSearchField(event.target.value)}>
+        <div className="flex items-center px-2 gap-2">
+            <input value={searchText} placeholder="Search..." onChange={(event) => setSearchText(event.target.value)} className="bg-zinc-700 border-2 border-zinc-600 rounded-md text-white px-1"></input>
+            <select name="searchField" value={searchField} onChange={(event) => setSearchField(event.target.value)} className="p-1 bg-linear-to-tl from-indigo-700 to-blue-200 text-indigo-50 rounded-md">
                 <option value="jobTitle">Job Title</option>
                 <option value="company">Company</option>
                 <option value="location">Location</option>
